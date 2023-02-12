2965c25d14a6dc35afc79b14c119ccfefe419787
TonyIvanov23 <59064960+TonyIvanov23@users.noreply.github.com>
2023-02-12 16:28:47 -0500

    got the autobalance to work but is still slow

--- src/main/java/frc/robot/subsystems/telemetry/commands/autoBalancePitch.java
+++ src/main/java/frc/robot/subsystems/telemetry/commands/autoBalancePitch.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.SubsystemManager;
 import frc.robot.subsystems.drivetrain.SwerveDrivetrain;
 import frc.robot.subsystems.telemetry.OzoneImu;
+import pabeles.concurrency.IntOperatorTask.Max;
 
 public class autoBalancePitch extends CommandBase {
   SwerveDrivetrain drivetrain;
@@ -29,11 +30,16 @@ public class autoBalancePitch extends CommandBase {
   double rollSpeed = 0;
   double pitch;
 
+  static final double MAX_ERROR = 19;
   double PITCH_PID = 0;
   double ROLL_PID = 0;
 
+  boolean isPitchAtSetpoint = false;
+  boolean isRollAtSetpoint = false;
+
   //safe p: 0.00585
-  PIDController pid = new PIDController(0.00585, 0, 0);
+  PIDController pidPitch = new PIDController(0.006, 0, 0.002269);
+  PIDController pidRoll = new PIDController(0.00585, 0, 0);
 
   /** Creates a new autoBalance. */
   public autoBalancePitch(SwerveDrivetrain drivetrain) {
@@ -52,22 +58,36 @@ public class autoBalancePitch extends CommandBase {
   @Override
   public void initialize() {
     pitch = pigeon.getPitch();
-    pid.setTolerance(TOLERANCE);
+    pidPitch.setTolerance(TOLERANCE);
+    pidPitch.setSetpoint(0);
+    pidRoll.setTolerance(TOLERANCE);
+    pidRoll.setSetpoint(0);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     pitch = pigeon.getPitch();
-    PITCH_PID = pid.calculate(pigeon.getPitch());
-    ROLL_PID = pid.calculate(pigeon.getRoll());
-    ChassisSpeeds speed = new ChassisSpeeds(pid.calculate(pigeon.getPitch()), pid.calculate(pigeon.getRoll()), 0);
-    drivetrain.drive(speed, false);
+    PITCH_PID = pidPitch.calculate(MathUtil.clamp(pigeon.getPitch(), pidPitch.getSetpoint() - MAX_ERROR, pidPitch.getSetpoint() + MAX_ERROR));
+    ROLL_PID = pidRoll.calculate(MathUtil.clamp(pigeon.getRoll(), pidRoll.getSetpoint() - MAX_ERROR, pidRoll.getSetpoint() + MAX_ERROR));
+    ChassisSpeeds speed = new ChassisSpeeds(PITCH_PID, ROLL_PID, 0);
+    if(!(pidPitch.atSetpoint() && pidRoll.atSetpoint())) {
+      drivetrain.drive(speed, false);
+    }
+    else{
+      ChassisSpeeds stopDrive = new ChassisSpeeds(0, 0, 0);
+      drivetrain.drive(stopDrive, false);
+    }
 
     previousPitch = pitch;
 
+    isPitchAtSetpoint = pidPitch.atSetpoint();
+    isRollAtSetpoint = pidRoll.atSetpoint();
+
     SmartDashboard.putNumber("PID Pitch Output", PITCH_PID);
     SmartDashboard.putNumber("PID Roll Output", ROLL_PID);
+    SmartDashboard.putBoolean("Is Pitch Ar Setpoint", isPitchAtSetpoint);
+    SmartDashboard.putBoolean("Is Roll At Setpoint", isRollAtSetpoint);
 
     //SmartDashboard.putNumber("Bang Bang Output", bangBang(pitch, TOLERANCE));
 
